698db4f665fe10c2e83afaf5e8979afec8be04ca
Vivica Ma <vivicacongma@gmail.com>
2020-02-10 04:50:49 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,19 +4,23 @@ def mario
   puts "phrase"
   phrase
 end
+mario 
 
 def toadstool
   puts status
   status
 end
+toadstool
 
 def link
   puts "It's Dangerous To Go Alone! Take This."
 end
+link
 
 def any_phrase(phrase)
   phrase = "Do A Barrel Roll!"
   puts "phrase"
   phrase
 end
+any_phrase
   
